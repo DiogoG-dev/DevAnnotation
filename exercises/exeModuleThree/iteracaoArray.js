24a38f92e90e5b7a4ffde8ff75e0f35a7a011bd5
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-02-11 21:58:24 -0300

    Some corrections

--- exercises/exeModuleThree/iteracaoArray.js
+++ exercises/exeModuleThree/iteracaoArray.js
@@ -6,33 +6,31 @@ const preco = [3.5, 5, 12, 7.2, 1];
 pessoas.forEach((value, index, array) => {
     console.log(value, index, array);
 });
-
 frutas.forEach((value, index, array) => {
     console.log(value.nome, index, array);
 });
 
-// Itera sobre todos os itens do array, podendo este criar um novo array, map
+// Itera sobre todos os itens do array, podendo este criar um novo array
 const returnMap = pessoas.map((value, index) => {
     return `${value} ${index}`
 })
 
-// Assim como Map, Reduce pode iterar sobre os itens e criar um novo array, porém neste contém um acumulador que armazena o item anterior.
+// Acumula o valor anterior para interagir com o atual
 const returnReduce = preco.reduce((acc, value) => {
     return acc + value
 }, 0) // 0 é o acumulado inicial
 
-
-// Para retornar o primeiro valor True de um array utiliza-se o find
+// Retorna o primeiro valor verdadeiro do array
 const returnFind = pessoas.find((value) => value)
 
-// Já para retorna a posição do primeiro valor True utiliza-se findIndex
+// Retorna a posição do primeiro valor verdadeiro do array
 const returnoFindIndex = pessoas.findIndex((value) => value)
 
-// O some retorna um boolean de acordo com os valores no array, se existir um valor True retorna true, se todos forem False retorna false
+// Retorna true quando existir um valor true e false quando todos forem false
 const returnoSome = pessoas.some((value) => value)
 
-// O every, assim como some, retorna um boolean de acordo com os valores no array, porém todos precisão ser True para retorna true e um False para false
+// Retorna true quando todos os valores forem true e false quando ao menos um for false
 const returnEvery = pessoas.every((value) => value)
 
-// Para retornar o valou especificado utiliza-se filter
+// Retorna o valor especificado
 const returnFilter = pessoas.filter((value) => value === 'Raissa')
